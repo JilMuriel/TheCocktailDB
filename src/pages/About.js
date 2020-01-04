3c7f920967e866d7daf2cfdac72358c05266535c
Vigil Boniol <VigilBoniol@NTWK-MBP.local>
2020-01-04 23:37:51 +0800

    Initial files for the app

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor amet meditation iceland marfa pork belly. Celiac brunch 90's lumbersexual squid ramps jean shorts before they sold out. Four loko offal pop-up post-ironic umami. Shaman gastropub beard tousled blue bottle plaid whatever activated charcoal XOXO taiyaki neutra hoodie seitan man bun williamsburg.
+      </p>
+  </section>;
+}
